0b2e9f7574d7c10f4401aa1c65a3fcd265151e62
Dominik Zogg <dominik.zogg@gmail.com>
2012-01-15 15:38:20 +0100

    complete recode, to get the possibility to add diffrent direct entries

--- TL_ROOT/system/modules/directentrys_theme/DirectEntries.php
+++ TL_ROOT/system/modules/directentrys_theme/DirectEntries.php
@@ -1,4 +1,4 @@
-<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+<?php if(!defined('TL_ROOT')) die('You cannot access this file directly!');
 
 /**
  * Contao Open Source CMS
@@ -21,75 +21,221 @@
  * Software Foundation website at <http://www.gnu.org/licenses/>.
  *
  * PHP version 5
- * @copyright  © Dominik Zogg 2011
- * @author     Dominik Zogg
- * @package    directentrys_theme
- * @license    LGPLv3
- * @filesource
+ * @copyright  Dominik Zogg 2011
+ * @author     Dominik Zogg <dominik.zogg@gmail.com>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
 
-class DirectEntrysTheme extends Backend
+class DirectEntries extends Backend
 {
     /**
-     * arrIcons
-     * @var array to show icons
+     * __construct
      */
-    var $arrIcons = array
-    (
-        'tl_style_sheet' => 'css',
-        'tl_module' => 'modules',
-        'tl_layout' => 'layout',
-    );
+    public function __construct()
+    {
+        // load libraries
+        $this->import("Database");
+        $this->import('BackendUser', 'User');
+    }
 
     /**
-     * injectThemes : HOOK
+     * inject
      * @param str $strContent rendered template
      * @param str $strTemplate template name
      * @return str rendered template
      */
-    public function injectThemes($strContent, $strTemplate)
+    public function inject($strContent, $strTemplate)
     {
-        // import objects
-        $this->import("Database");
-        $this->import('BackendUser', 'User');
-        // check if this is the backend template and the user got the permission for this action
-        if($strTemplate == 'be_main' && ($this->User->isAdmin || $this->User->hasAccess('themes', 'modules')))
+        if($strTemplate == 'be_main')
         {
-            // open list
-            $strToAdd = '<ul>';
-            // get all existing themes
-            $objResult = $this->Database->query("SELECT id,name FROM tl_theme ORDER BY name");
-            while($row = $objResult->fetchAssoc())
+            // themes
+            $arrThemes = $this->_prepareThemeArray();
+            $strThemeHtml = $this->_buildHtml($arrThemes);
+            $strContent = $this->_addContent('design', 'themes', $strThemeHtml, $strContent);
+
+            // pages
+            $arrPages = $this->_preparePageArray();
+            $strPageHtml = $this->_buildHtml($arrPages);
+            $strContent = $this->_addContent('design', 'page', $strPageHtml, $strContent);
+        }
+        // return rendered template
+        return($strContent);
+    }
+
+    /**
+     * _prepareThemeArray
+     * @return boolean|array the theme array
+     */
+    protected function _prepareThemeArray()
+    {
+        // check permission
+        if($this->User->isAdmin || $this->User->hasAccess('themes', 'modules'))
+        {
+            // get all existing themes
+            $objThemes = $this->Database->query("SELECT id,name FROM tl_theme ORDER BY name");
+
+            // if there is at minimum one theme
+            if($objThemes->numRows)
             {
-                // prepare the theme name
-                $strThemeName = strlen($row['name']) > 10 ? substr($row['name'], 0, 8) . '...' : $row['name'];
-                // prepare list element for the theme
-                $strToAdd .= '<li><span style="padding-left: 15px;">';
-                // foreach icon to build a link for
-                foreach($this->arrIcons as $strIconTable => $strIcon)
+                // prepare array return
+                $arrReturn = array();
+
+                // set counter
+                $intCounter = 1;
+
+                // set icons array
+                $arrIcons = array
+                (
+                    'css' => 'tl_style_sheet',
+                    'modules' => 'tl_module',
+                    'layout' => 'tl_layout',
+                );
+
+                // do this foreach theme
+                while($objThemes->next())
                 {
-                    // check if the user got the permission for this action
-                    if($this->User->isAdmin || $this->User->hasAccess($strIcon, 'themes'))
+                    // set the theme url and title
+                    $arrReturn[$intCounter]['name']['url'] = 'contao/main.php?do=themes&act=edit&id=' . $objThemes->id;
+                    $arrReturn[$intCounter]['name']['title'] = strlen($objThemes->name) > 10 ? substr($objThemes->name, 0, 8) . '...' : $objThemes->name;
+
+                    // foreach icons
+                    foreach($arrIcons as $strIcon => $strTableName)
                     {
-                        $strToAdd .= '<a style="padding-right: 2px;" href="contao/main.php?do=themes&table=' . $strIconTable . '&id=' . $row['id'] . '">';
-                        $strToAdd .= '<img src="system/themes/default/images/' . $strIcon . '.gif" alt="' . $strIcon . '" />';
-                        $strToAdd .= '</a>';
+                        // check detail permissions
+                        if($this->User->isAdmin || $this->User->hasAccess($strIcon, 'themes'))
+                        {
+                            // set the icon url and title
+                            $arrReturn[$intCounter]['icons'][$strIcon]['url'] = 'contao/main.php?do=themes&id=' . $objThemes->id . '&table=' . $strTableName;
+                            $arrReturn[$intCounter]['icons'][$strIcon]['title'] = $strIcon;
+                        }
+                    }
+                    // add one to counter
+                    $intCounter++;
+                }
+                // return array
+                return($arrReturn);
+            }
+        }
+        // return false if theres no permission or no themes
+        return(false);
+    }
+
+    /**
+     * _preparePageArray
+     * @return boolean|array the page array
+     */
+    protected function _preparePageArray()
+    {
+        // check permission
+        if($this->User->isAdmin || $this->User->hasAccess('page', 'modules'))
+        {
+            // get all existing root pages
+            $objPages = $this->Database->query("SELECT id,title FROM tl_page WHERE type = 'root' ORDER BY title");
+
+            // if there is at minimum one page
+            if($objPages->numRows)
+            {
+                // prepare array return
+                $arrReturn = array();
+
+                // set counter
+                $intCounter = 1;
+
+                // do this foreach page
+                while($objPages->next())
+                {
+                    // set the icon url and title
+                    $arrReturn[$intCounter]['icons']['page']['url'] = 'contao/main.php?do=page&node=' . $objPages->id;
+                    $arrReturn[$intCounter]['icons']['page']['title'] = 'page';
+
+                    // set the page url and title
+                    $arrReturn[$intCounter]['name']['url'] = 'contao/main.php?do=page&node=' . $objPages->id;
+                    $arrReturn[$intCounter]['name']['title'] = strlen($objPages->title) > 15 ? substr($objPages->title, 0, 13) . '...' : $objPages->title;
+
+                    // add one to counter
+                    $intCounter++;
+                }
+                // return array
+                return($arrReturn);
+            }
+        }
+        // return false if theres no permission or no page
+        return(false);
+    }
+
+    /**
+     * _buildHtml
+     * @param boolean|array $arrPreparedArray the array to build html from
+     * @return string html
+     */
+    protected function _buildHtml($arrPreparedArray)
+    {
+        // html string
+        $strHtml = '';
+
+        // if the input is an array
+        if(is_array($arrPreparedArray))
+        {
+            // add list open tag
+            $strHtml .= '<ul>';
+
+            // forech list element
+            foreach($arrPreparedArray as $arrListElement)
+            {
+                // add list element open tag
+                $strHtml .= '<li style="padding-left: 15px;">';
+
+                // check for icons
+                if(isset($arrListElement['icons']) && is_array($arrListElement['icons']))
+                {
+                    // foreach icon
+                    foreach($arrListElement['icons'] as $arrTitleAndUrl)
+                    {
+                        // add icon link
+                        $strHtml .= '<a style="padding-right: 2px;" title="' . $arrTitleAndUrl['title'] . '" href="' . $arrTitleAndUrl['url'] . '">';
+                        $strHtml .= '<img style="margin:0; padding: 0; width: 16px; height: 16px;" src="system/themes/default/images/' . $arrTitleAndUrl['title'] . '.gif" alt="' . $arrTitleAndUrl['title'] . '" />';
+                        $strHtml .= '</a>';
                     }
                 }
-                // link for editing the theme metainformation
-                $strToAdd .= '<a href="contao/main.php?do=themes&act=edit&id=' . $objResult->id . '">' . $strThemeName . '</a>';
-                $strToAdd .= '</span></li>';
+
+                // check for name
+                if(isset($arrListElement['name']) && is_array($arrListElement['name']))
+                {
+                    // add name link
+                    $strHtml .= '<a title="' . $arrListElement['name']['title'] . '" href="' . $arrListElement['name']['url'] . '">';
+                    $strHtml .= $arrListElement['name']['title'];
+                    $strHtml .= '</a>';
+                }
+
+                // add list element close tag
+                $strHtml .= '</li>';
             }
-            // close list
-            $strToAdd .= '</ul>';
-            // inject the elements
-            $strContent = preg_replace
+
+            // add list close tag
+            $strHtml .= '</ul>';
+        }
+
+        // return html
+        return $strHtml;
+    }
+
+    /**
+     * _addContent
+     * @param string $strToGroup for example design
+     * @param string $strToElement for example themes
+     * @param string $strToAdd html to add
+     * @param string $strContent the old content
+     * @return string the new content
+     */
+    protected function _addContent($strToGroup, $strToElement, $strToAdd, $strContent)
+    {
+        return(
+            preg_replace
             (
-                '/(\<li.*?id="design".*?\>[\S\s]*?\<ul.*?\>[\S\s]*?\<li.*?\>[\S\s]*?\<a.*?themes[\S\s]*?\>.*?\<\/a\>)([\S\s]*?\<\/li\>[\S\s]*?\<\/ul\>[\S\s]*?\<\/li\>)/',
+                '/(\<li.*?id="' . $strToGroup .'".*?\>[\S\s]*?\<ul.*?\>[\S\s]*?\<li.*?\>[\S\s]*?\<a.*?' . $strToElement . '[\S\s]*?\>.*?\<\/a\>)([\S\s]*?\<\/li\>[\S\s]*?\<\/ul\>[\S\s]*?\<\/li\>)/',
                 '${1}' . $strToAdd . '${2}',
                 $strContent
-            );
-        }
-        return($strContent);
+            )
+        );
     }
 }
